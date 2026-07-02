fe34196b06187d22bc75af347499542ce741daa2
Hannes Lundin <hanneslundin@hotmail.com>
2026-07-02 08:32:13 +0200

    day 11

---
+++ src/main.js
@@ -0,0 +1,34 @@
+console.log("Hello TypeScript!!");
+const taskName = "Learn TypeScript";
+console.log(taskName);
+const priority = 1;
+const completed = true;
+const taskName2 = "Learch C#";
+const priority2 = 2;
+const completed2 = true;
+const taskName3 = "Learn Ruby";
+const priority3 = 3;
+const completed3 = false;
+const completedTasks = 1;
+const totalTasks = 3;
+const completionRate = completedTasks / totalTasks * 100;
+console.log(`
+Task: ${taskName}
+Priority: ${priority}
+Completed: ${completed}
+`);
+console.log(`
+Task: ${taskName2}
+Priority: ${priority}
+Completed: ${completed2}
+`);
+console.log(`
+    Task: ${taskName3}
+    Priority: ${priority}
+    Completed: ${completed}
+`);
+console.log(`
+        completionRate: ${completionRate}%
+    `);
+export {};
+//# sourceMappingURL=main.js.map
---
+++ src/task-tracker/render.ts
@@ -0,0 +1,54 @@
+import { app, getTaskArray } from "./types";
+import { toggleTask, deleteTask } from "./tasks";
+
+export function renderTasks(): void {
+    if (app) {
+        app.innerHTML = "";
+    }
+
+    for (const task of getTaskArray()) {
+        const card = document.createElement("div");
+        card.classList.add("task");
+
+        if (task.priority === "high") {
+            card.classList.add("high-priority");
+        }
+
+        if (task.status === "completed") {
+            card.classList.add("completed");
+        }
+       
+        const title = document.createElement("h3");
+        title.textContent = task.name;
+
+        const status = document.createElement("p");
+        status.textContent = `Status: ${task.status}`
+
+        const priority = document.createElement("p");
+        priority.textContent = `Priority: ${task.priority}`;
+
+        const completeButton = document.createElement("button");
+        completeButton.classList.add("btn");
+        completeButton.textContent = task.status === "pending" ? "Complete" : "Undo";
+        completeButton.addEventListener("click", () => {
+            toggleTask(task.id);
+        })
+
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("btn");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", () => {
+            deleteTask(task.id);
+        })
+
+        card.append(
+            title,
+            status,
+            priority,
+            completeButton,
+            deleteButton
+        );
+
+        app?.append(card);
+    } 
+}
